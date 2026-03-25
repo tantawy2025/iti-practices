9d604501e1e33f59380bc636b91c7f232b4b08a2
Mohamed Tantawy <mtantawy2025@gmail.com>
2026-03-26 01:15:01 +0200

    add page main compnents

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './components/header/header';
+import { NavBar } from './components/nav-bar/nav-bar';
+import { SideBar } from './components/side-bar/side-bar';
+import { Products } from './components/products/products';
+import { Footer } from './components/footer/footer';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Header, NavBar, SideBar, Products, Footer],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/side-bar/side-bar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  standalone: false,
+  templateUrl: './side-bar.html',
+  styleUrl: './side-bar.css',
+})
+export class SideBar {}
